8c7b7b8075ba69e0b68ee11c630187ac9aa10996
daochao.qdc <daochao.qdc@alibaba-inc.com>
2016-06-27 18:05:22 +0800

    add token and ENCODING_AES_KEY

--- src/com/alibaba/dingtalk/openapi/demo/Env.java
+++ src/com/alibaba/dingtalk/openapi/demo/Env.java
@@ -1,12 +1,11 @@
 package com.alibaba.dingtalk.openapi.demo;
 
 
-
 public class Env {
 	
 	public static final String OAPI_HOST = "https://oapi.dingtalk.com";
 	public static final String OA_BACKGROUND_URL = ""; 
-	public static final String CORP_ID = "ding1357db912fad244f";
+	public static final String CORP_ID = "ding56df9241fa0ffd1f";
 	
 	public static final String SECRET = "";
 	public static final String SSO_Secret = "";
@@ -19,7 +18,7 @@ public class Env {
 	public static final String CREATE_SUITE_KEY = "suite4xxxxxxxxxxxxxxx";
 	public static final String SUITE_KEY = "";
 	public static final String SUITE_SECRET = "";
-	public static final String TOKEN = "";
-	public static final String ENCODING_AES_KEY = "";
+	public static final String TOKEN = "waitbutwhytoken";
+	public static final String ENCODING_AES_KEY = "txtrzd6g0dgu06j1y4c5ahfvd49grnk401q8f214qmr";
 	
 }
